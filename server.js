eaeee58911fda7d428c063343c2b01069c378e98
Luis Hernandez <rojoclifford@gmail.com>
2018-04-25 00:15:47 -0400

    Update server.js
    Added working server.js file

--- server.js
+++ server.js
@@ -1,97 +1,32 @@
-// Dependencies below
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var express = require('express');
-var bodyParser = require('body-parser');
-// requiring note and article models
-var Article = require("./models/Articles.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// this is for heroku deployment
-var PORT = process.env.PORT || 3002
-
-
-// initialize express
-var app = express();
-
-
-// logging using Morgan
-app.use(logger('dev'));
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// uses body parser with the app
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// Make public a static dir
-app.use(express.static('public'));
+// Add routes, both API and view
+app.use(routes);
 
-// var dbConnect =
-//     process.env.MONGODB_URI ||
-//     process.env.MONGOHQ_URL ||
-//     'mongodb://localhost/ReactMovieNews';
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
+  {
+    useMongoClient: true
+  }
+);
 
-// // mongoDB_URI : mongodb://heroku_f727w5fr:epf9p9fd2brsmmfop5246
-// //vu6r3@ds115583.mlab.com:15583/heroku_f727w5fr
-
-// // connect to the mongo db
-// mongoose.connect(dbConnect, 
-//   {useMongoClient: true }
-// );
-
-// var db = mongoose.connection;
-
-// // any mongoose errors
-// db.on('error', function(error) {
-//   console.log('Mongoose Error: ', error)
-// });
-// // success messages 
-// db.on('open', function() {
-//   console.log('Mongoose connection successful');
-// });
-
-// get, post, delete routes
-app.get('/', function (req, res) {
-    res.sendFile('./public/index.html');
-})
-
-app.get('/api/saved', function (req, res) {
-    Article.find({})
-        .exec(function (err, doc) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(doc);
-            }
-        })
-});
-
-app.post('/api/saved', function (req, res) {
-    const newArticle = new Article({
-        title: req.body.title,
-        date: req.body.date,
-        url: req.body.url
-    });
-
-    newArticle.save(function (err, doc) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            res.json(doc);
-        }
-    });
-});
-
-app.delete('/api/saved/:id', function (req, res) {
-    Article.find({'_id': req.params.id}).remove()
-        .exec(function (err, doc) {
-            res.send(doc);
-        });
-});
-
-// app listen on port 3000
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
